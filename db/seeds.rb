6ed36210e8664ec6ab5894b3cab0abc7c47b6c81
John Mazzucco <John.Mazzucco@gmail.com>
2015-05-06 17:47:42 -0400

    Fix bug in database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-100.times do
-	Product.create!(
-		name: Faker::Company.catch_phrase,
-		description: Faker::Company.bs,
-		price_in_cents: rand(25..2500)
-	)
+# 100.times do
+# 	Product.create!(
+# 		name: Faker::Company.catch_phrase,
+# 		description: Faker::Company.bs,
+# 		price_in_cents: rand(25..2500)
+# 	)
+# end
+
+User.destroy_all
+Product.destroy_all
+
+User.create!({
+	email: "example@bitmakerlabs.com",
+	name: "Example User",
+	password: "pass",
+	password_confirmation: "pass"
+})
+
+100.times do |i|
+
+Product.create({
+  name: "Product#{i}",
+  description: "Description#{i}",
+  price_in_cents: i
+  }
+)
 end
